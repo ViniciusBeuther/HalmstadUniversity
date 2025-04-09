01ef4965dc433216e7e838e8c6197413ff1ebea1
Vinicius E Beuther <vinicius.beuther15@gmail.com>
2025-04-09 03:22:21 -0300

    Added comments to question 14

--- Advanced_OOP/Assignments/src/main/java/Assignment_01/Question_14/Main.java
+++ Advanced_OOP/Assignments/src/main/java/Assignment_01/Question_14/Main.java
@@ -2,13 +2,16 @@ package Assignment_01.Question_14;
 
 public class Main {
     public static void main(String[] args){
+        // Initial list of words for testing class
         String[] words = {"Duck", "Bear", "Fish", "OOP", "AAA"};
 
+        // Instantiate our filter class, which is based on the Filter Interface
         ShortStringFilter shortFilter = new ShortStringFilter();
 
+        // Call the method to filter the words with size <= 3
         String[] goodWords = filter(words, shortFilter);
 
-        // Loop thru the words with size <= 3
+        // Loop list to print words
         for(String word : goodWords){
             System.out.println(word);
         }
@@ -18,6 +21,7 @@ public class Main {
     public static String[] filter(String[] a, Filter f){
         int counter = 0;
 
+        // Count how many good words it has to initialize the output array with the correct size
         for(String word : a){
             if( f.accept(word) ){
                 counter++;
@@ -27,6 +31,7 @@ public class Main {
         String[] acceptedWords = new String[counter];
         int index = 0;
 
+        // Looping over the words and adding them to the output string[]
         for(String word : a){
             if( f.accept(word) ){
                 acceptedWords[index] = word;
