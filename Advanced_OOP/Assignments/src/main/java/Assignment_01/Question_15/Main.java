1446f1d866112df14fdfd08be45f111b9d757e3f
Vinicius E Beuther <vinicius.beuther15@gmail.com>
2025-04-09 04:48:23 -0300

    Updated question 15 comments

--- Advanced_OOP/Assignments/src/main/java/Assignment_01/Question_15/Main.java
+++ Advanced_OOP/Assignments/src/main/java/Assignment_01/Question_15/Main.java
@@ -5,14 +5,17 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.geom.Ellipse2D;
 
+// Class to handle the icon drawing/color
 class ShapeDrawing extends JPanel{
     private Color color = Color.WHITE;
 
+    // Method to define the circle color
     public void setColor(Color color){
         this.color = color;
         repaint();
     }
 
+    // Method that draw the ellipse
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
@@ -23,6 +26,7 @@ class ShapeDrawing extends JPanel{
         g2.draw(ellipse);
     }
 
+    // Adjust the window size
     @Override
     public Dimension getPreferredSize(){
         return new Dimension(200, 200);
@@ -35,23 +39,29 @@ public class Main {
         // Instantiate a frame, set to be visible and define the default operation when hits close
         JFrame frame = new JFrame();
 
+        // Initialize ellipse shaping
         ShapeDrawing sd = new ShapeDrawing();
 
+        // Define buttons
         JButton redButton = new JButton("Red");
         JButton blueButton = new JButton("Blue");
         JButton greenButton = new JButton("Green");
 
+        // Add event listener to the buttons
         redButton.addActionListener(event -> sd.setColor(Color.RED));
         greenButton.addActionListener(event -> sd.setColor(Color.GREEN));
         blueButton.addActionListener(event -> sd.setColor(Color.BLUE));
 
+        // Define layout
         frame.setLayout(new FlowLayout());
 
+        // Add components to the frame
         frame.add(redButton);
         frame.add(greenButton);
         frame.add(blueButton);
         frame.add(sd);
 
+        // Start the frame and define the default function to close window
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
